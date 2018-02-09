8cd084b3dfc82a8b93c7b7d0464b7cbcc4f39730
Sunrise <oksvip@sina.com>
2018-02-09 18:22:31 +0800

    整合adminlte

---
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -0,0 +1,63 @@
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="menu" class="form-control" placeholder="搜索...">
+                <span class="input-group-btn">
+              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+              </button>
+            </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <!-- Optionally, you can add icons to the links -->
+            <li class="active"><a href="{{ route('admin.index') }}"><i class="fa fa-link"></i> <span>首页</span></a></li>
+        </ul>
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">用户模块</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="#"><i class="fa fa-link"></i> <span>用户管理</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>用户标签管理</span></a></li>
+        </ul>
+        <!-- /.sidebar-menu -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">文章模块</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="#"><i class="fa fa-link"></i> <span>文章管理</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>发布文章</span></a></li>
+        </ul>
+        <!-- /.sidebar-menu -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">统计管理</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li><a href="#"><i class="fa fa-link"></i> <span>用户统计</span></a></li>
+            <li><a href="#"><i class="fa fa-link"></i> <span>用户标签统计</span></a></li>
+            <li class="treeview">
+                <a href="#"><i class="fa fa-link"></i> <span>排行统计</span>
+                    <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#">扫码排行</a></li>
+                    <li><a href="#">登录排行</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>@yield('title', '首页') - {{ config('app.name') }}后台管理</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('static/admin/css/app.css') }}">
+
+    <!-- IE8 support of HTML5 elements and media queries -->
+    @include('admin.layouts._support')
+
+</head>
+<!--
+BODY TAG OPTIONS:
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+    <!-- Main Header -->
+    @include('admin.layouts._header')
+
+    <!-- Left side column. contains the logo and sidebar -->
+    @include('admin.layouts._sidebar')
+
+    <!-- Content Wrapper. Contains page content -->
+    @yield('content')
+
+    <!-- Main Footer -->
+    @include('admin.layouts._footer')
+
+    <!-- Control Sidebar -->
+    @include('admin.layouts._control')
+</div>
+<!-- ./wrapper -->
+
+<!-- JS -->
+<script src="{{ asset('static/admin/js/app.js') }}"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
